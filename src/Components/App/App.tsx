183e7dbc6a02aa0b90c64b438deec6f46169b025
Artan Myrtolli <artanmyrtolli@gmail.com>
2022-07-11 17:48:24 -0400

    Set up the search form with livesearch and add new component submissionForm

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from 'react';
 // import { Route } from 
 import './App.css';
 import Nav from '../../Components/Nav/Nav'
-import Form from '../Form/Form';
 import { User } from '../../interfaces'
 import { user1 } from '../../mockData'
 import { Route } from 'react-router-dom'
+import SearchForm from '../Form/SearchForm';
 
 const App = () => {
   const [user, setUser] = useState<User>()
@@ -19,12 +19,9 @@ const App = () => {
     <div className="App">
       {user ? <Nav name={user.name} /> : <Nav name={'No user found'} />}
       <Route exact path='/add-new'>
-        {user ? <Form userID={user.id} /> : <Form userID={0} />}
-
+        {user ? <SearchForm userID={user.id} /> : <SearchForm userID={0} />}
       </Route>
-
     </div>
-
   );
 }
 
---
+++ src/Components/Form/SubmissionForm.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type MedProps = {
+    chosenMedicine: string
+}
+
+const SubmissionForm: React.FC<MedProps> = ({chosenMedicine}) => {
+    return (
+        <>
+            <p>Submission Form Starts Here</p>
+        </>
+    )
+}
+
+
+
+export default SubmissionForm
