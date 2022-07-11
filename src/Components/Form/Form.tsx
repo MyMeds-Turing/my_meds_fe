d1b7e523edd01f71fadd0f7a60ec74cf03922445
Artan Myrtolli <artanmyrtolli@gmail.com>
2022-07-11 17:01:32 -0400

    Create a search field on form with live results

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { ReactComponentElement, ReactNode, useEffect, useState } from 'react';
 import './Form.css';
 
 type UserProps = {
@@ -6,22 +6,44 @@ type UserProps = {
   }
 
 const Form: React.FC<UserProps> =({ userID }) => {
-  const [medicine, setMedicine] = useState('')
+  const [chosenMedicine, setChosenMedicine] = useState<string>('')
   const [allMeds, setAllMeds] = useState<Array<string>>([])
+  const [displayResults, setDisplayResults] = useState<boolean>(false)
+  const [searchResults, setSearchResults] = useState<ReactNode[]>([])
   
-
-
-
   useEffect(() => {
     fetch('https://rxnav.nlm.nih.gov/REST/displaynames.json')
       .then(response => response.json())
-      .then(data => console.log(data))   
+      .then(data => setAllMeds(data.displayTermsList.term))
   }, [])
+  
+  const displaySearchResults = (userInput: string) => {
+    setDisplayResults(true)
+      const userSearchMeds = allMeds.filter(med => med.includes(userInput))
+      const formattedResults = userSearchMeds.map(userMed => {
+        return <p className='form__search-result' key={userMed} onClick={() => handleUserChoice(userMed)}>{userMed}</p>
+      })
+      setSearchResults(formattedResults)
+  }
+
+  const handleUserChoice = (userChoice: string) => {
+    setChosenMedicine(userChoice)
+    setDisplayResults(false)
+  }
 
   return (
     <div>
         <p>form will be here</p>
         <p>{userID}</p>
+        <div className='form__medicine-lookup'>
+          <h2>Search Medicine</h2>
+          <input type='text' placeholder='Medicine Name' onChange={event => displaySearchResults(event.target.value)}/>
+          <button>Submit</button>
+        </div>
+        {displayResults && <div className='form__live-search-results-box'>
+            {searchResults}
+        </div>}
+        {chosenMedicine && <h2>{chosenMedicine}</h2>}
     </div>
   );
 }
